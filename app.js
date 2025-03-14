0268897a8c3d8baa8b010dd05b6c4315c22ba105
Rafael <rahevaco@gmail.com>
2025-03-14 19:11:48 -0300

    validar la entrada

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let amigos = []
 //capturar el valor del campo de entrada
 function agregarAmigo() {
-    let ingresarAmigo = document.getElementById("amigo").value;
-    
-    
+    let ingresarAmigo = document.getElementById("amigo");
+    let amigo = ingresarAmigo.value;
+
+    if(!amigo) {
+        alert("por favor inserte un nombre");
+        return;
+    }
 }
