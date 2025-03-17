5ffec6d411a13edb988586483d37205c48ef8c10
Rafael <rahevaco@gmail.com>
2025-03-17 20:08:23 -0300

    agregar elementos a la lista

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-let amigos = []
+let amigo = []
 //capturar el valor del campo de entrada
 function agregarAmigo() {
     let ingresarAmigo = document.getElementById("amigo");
-    let amigo = ingresarAmigo.value;
+    let amigos = ingresarAmigo.value
 //validando entrada
-    if(amigo == false) {
+    if(!amigos) {
         alert("por favor inserte un nombre");
         return;
     }
     //actualizando el array de amigos
-    amigos.push(amigo);
+    amigo.push(amigos);
     //limpiar el campo de entrada
     ingresarAmigo.value = "";
+    ingresarAmigo.focus();
+
+    listaDeAmigos();
     
 }
 
 function listaDeAmigos() {
     //obtener el elmento de la lista
-    let LaListaAmigos =document.getElementById("listaAmigos");
+    let listaAmigos = document.getElementById("listaAmigos");
     //limpiar lista exiztente
-    LaListaAmigos.innerHTML = "";
+    listaAmigos.innerHTML = "";
+
+    //iterar sobre el arreglo
+    for (let i = 0; i < amigo.length; i++) {
+        let lista = document.createElement("li");
+        //agregar elementos a la lista
+        lista.textContent = amigo[i];
+        listaAmigos.appendChild(lista);
+    }
 
     
 }
